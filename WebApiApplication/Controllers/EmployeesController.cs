7decf5de00944a5db3517610aa323848c8068bc7
Nazar.M <inazar.m@gmail.com>
2020-08-12 00:20:23 +0500

    Connection String Updated and Data Get
    get Data By Api

---
+++ WebApiApplication/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeesDataAccess;
+
+
+namespace WebApiApplication.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (employeeDBEntities db=new employeeDBEntities())
+            {
+                return db.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (employeeDBEntities db = new employeeDBEntities())
+            {
+                return db.Employees.FirstOrDefault(e=>e.Id==id);
+            }
+        }
+
+
+    }
+}
